2514a9aa635e4c2732a077f093b6d8f774c4be24
Gustavo Leck <gustavoleck@hotmail.com>
2025-01-15 22:08:48 -0300

    First Deploy

--- crm/account/request.go
+++ crm/account/request.go
@@ -1,23 +1,28 @@
 package account
-
+import "fmt"
 //Get is a function that returns the account details
 func read() any {
+	fmt.Println("get")
 	return "get"
 }
 
 func create() any {
+	fmt.Println("create")
 	return "create"
 }
 
 func delete() any {
+	fmt.Println("delete")
 	return "delete"
 }
 
 func update() any {
+	fmt.Println("update")
 	return "update"
 }
 
 //Busca conta
 func search() any {
+	fmt.Println("saerch")
 	return "search"
 }
