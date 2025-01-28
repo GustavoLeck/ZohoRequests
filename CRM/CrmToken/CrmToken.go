36fc9c08eee84b4939b4f81d81b116a0399aca89
Gustavo Leck <gustavoleck@hotmail.com>
2025-01-28 10:59:34 -0300

    feat(Finaliza modulo de tokens do CRM)

--- CRM/CrmToken/CrmToken.go
+++ CRM/CrmToken/CrmToken.go
@@ -4,35 +4,47 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/robfig/cron"
 )
 
 var cliId string
 var cliSecret string
 var rToken string
-var AcessToken string
+var acToken string
 
 func SetParameters(clientId string, clientSecret string, refreshToken string) {
 	cliId = clientId
 	cliSecret = clientSecret
 	rToken = refreshToken
 	UpdateAcessToken()
+	c := cron.New()
+	c.AddFunc("40 * * * *", func() {
+		fmt.Println("Atualizando o Acess Token", time.Now().Format("2006-01-02 15:04:05"))
+		UpdateAcessToken()
+	})
+	c.Start()
 }
 
 func UpdateAcessToken() {
 	res, err := http.Post("https://accounts.zoho.com/oauth/v2/token?grant_type=refresh_token&refresh_token="+rToken+"&client_id="+cliId+"&client_secret="+cliSecret, "application/x-www-form-urlencoded", nil)
 	if err != nil {
-		fmt.Println("Erro ao gerar token: " + err.Error())
-		return
+		panic("Erro ao tentar obter o access_token: " + err.Error())
 	}
 	defer res.Body.Close()
 	var result map[string]interface{}
 	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
-		fmt.Println("Erro ao decodificar o JSON: " + err.Error())
-		return
+		panic("Erro ao decodificar o JSON: " + err.Error())
 	}
 	if accessToken, ok := result["access_token"].(string); ok {
-		AcessToken = accessToken
-	} else {
-		fmt.Println("Erro: access_token não encontrado na resposta")
+		acToken = accessToken
 	}
+}
+
+func GetToken() (string, error) {
+	if acToken == "" {
+		panic("AcessToken não foi gerado")
+	}
+	return acToken, nil
 }
--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
-	"github.com/GustavoLeck/ZohoRequests/CRM/CrmToken"
+	"github.com/GustavoLeck/ZohoRequests/crm/CrmToken"
 )
 
 func main() {
-	fmt.Println("Teste")
-	//1000.86dacc2e6446f9828360b1a066ceb0fd.61ad74ae521b20f17aeab0bf193ea542
 	CrmToken.SetParameters("1000.QN3NAUYJWDCNP8U8SQEQCNPV34A46W", "a6ae958f0793193ec7fc7d95423ae247bbd0fbbd24", "1000.86dacc2e6446f9828360b1a066ceb0fd.61ad74ae521b20f17aeab0bf193ea542")
-	fmt.Println(CrmToken.AcessToken)
-	CrmToken.UpdateAcessToken()
-	fmt.Println(CrmToken.AcessToken)
+	HandleRequest()
 }
+
+func HandleRequest() {
+	fmt.Println("Server is running on port 8000")
+	// http.HandleFunc("/", Home)
+	http.ListenAndServe(":8000", nil)
+}
+
+// func Home(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Print(w, "Home Page")
+// }
